84898b1ceb0645a852f39586b0dfa36738345d54
Danil767 <danil.an.2024@mail.ru>
2024-03-11 13:32:43 +0600

    props

--- src/components/todo-list-item.jsx
+++ src/components/todo-list-item.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const TodoListItem = () => {
-    return <span>Drink Coffee</span>
+const TodoListItem = ({ label, important = false }) => {
+
+    const myStyle = {
+        color: important ? 'red' : 'black'
+    }
+
+    return <span style={myStyle}>{ label }</span>
 }
 
 export default TodoListItem
